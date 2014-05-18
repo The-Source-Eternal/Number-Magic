6d29053fe9890276e423d90dee638e5501fa5c06
knod <knodding@gmail.com>
2014-05-18 17:23:22 -0400

    Code Mirror editor now appears, filling the window

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /*
 	05/18/2014
 	Sources:
+	(1) http://codemirror.net
 */
+
+// Code mirror is added in javascript mode
+var codeEditor = CodeMirror(document.body, {
+	value: "function myScript(){return 100;}\n",
+});
